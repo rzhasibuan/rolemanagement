a47361899a78222a046c17248748b1de000bf50e
loop <rzhasibuan@gmail.com>
2020-09-07 11:07:45 +0700

    migration

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    /**
+     * Control if the seeder should create a user per role while seeding the data.
+     */
+    'create_users' => true,
+
+    /**
+     * Control if all the laratrust tables should be truncated before running the seeder.
+     */
+    'truncate_tables' => true,
+
+    'roles_structure' => [
+        'superadmin' => [
+            'users' => 'c,r,u,d',
+            'articles' => 'c,r,u,d',
+            'profile' => 'r,u'
+        ],
+        'admin' => [
+            'users' => 'c,r,u,d',
+            'articles' => 'r,u',
+            'profile' => 'r,u'
+        ],
+        'user' => [
+            'profile' => 'r,u',
+            'articles' => 'c,r,u,d',
+        ],
+        
+    ],
+
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete'
+    ]
+];
