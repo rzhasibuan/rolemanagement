459ce56a714528e0bd04537a89787eb09664d2c3
loop <rzhasibuan@gmail.com>
2020-09-14 16:04:44 +0700

    crud user

---
+++ app/Traits/FlashAlert.php
@@ -0,0 +1,57 @@
+<?php 
+
+namespace App\Traits;
+
+trait FlashAlert
+{
+    public function alertCreated()
+    {
+        return [
+            'type' => 'success',
+            'message' => 'Data successfully created!'
+        ];
+    }
+    
+    public function alertUpdate()
+    {
+        return [
+            'type' => 'success',
+            'message' => 'Data successfully updated'
+        ];
+    }
+
+    public function alertDeleted()
+    {
+        return [
+            'type' => 'success',
+            'message' => 'Data successfully deleted'
+        ];
+    }
+
+    public function alertNotFound()
+    {
+        return [
+            'type' => 'warning',
+            'message' => 'Data not found'
+        ];
+    }
+
+    public function alertDanger()
+    {
+        return [
+            'type' => 'danger',
+            'message' => 'Something wrong'
+        ];
+    }
+
+    public function permissionDenied()
+    {
+        return [
+            'type' => 'danger',
+            'message' => 'you don`t have permission to access!'
+        ];   
+    }
+
+
+
+}
